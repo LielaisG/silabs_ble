99177bfc25f3d2d9f6bdc5b0e362cf2694dfa777
LielaisG <fridenbergs.gatis@gmail.com>
2023-07-17 16:27:28 +0300

    Manufacturer & FW version added

--- autogen/gatt_db.c
+++ autogen/gatt_db.c
@@ -41,7 +41,7 @@ GATT_DATA(sli_bt_gattdb_attribute_chrvalue_t gattdb_attribute_field_23) = {
 GATT_DATA(sli_bt_gattdb_attribute_chrvalue_t gattdb_attribute_field_21) = {
   .properties = 0x02,
   .max_len = 5,
-  .data = { 0x30, 0x2e, 0x30, 0x2e, 0x30, },
+  .data = { 0x31, 0x2e, 0x30, 0x00, 0x00, },
 };
 GATT_DATA(sli_bt_gattdb_attribute_chrvalue_t gattdb_attribute_field_19) = {
   .properties = 0x02,
@@ -54,8 +54,8 @@ GATT_DATA(sli_bt_gattdb_attribute_chrvalue_t gattdb_attribute_field_17) = {
   .data = { 0x63, 0x75, 0x73, 0x74, 0x6f, 0x6d, 0x5f, 0x62, 0x72, 0x64, },
 };
 GATT_DATA(const sli_bt_gattdb_value_t gattdb_attribute_field_15) = {
-  .len = 12,
-  .data = { 0x53, 0x69, 0x6c, 0x69, 0x63, 0x6f, 0x6e, 0x20, 0x4c, 0x61, 0x62, 0x73, }
+  .len = 10,
+  .data = { 0x4d, 0x65, 0x73, 0x68, 0x20, 0x67, 0x72, 0x6f, 0x75, 0x70, }
 };
 GATT_DATA(const sli_bt_gattdb_value_t gattdb_attribute_field_13) = {
   .len = 2,
