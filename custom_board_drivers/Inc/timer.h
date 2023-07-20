984cfed030882432a77b298e3976fbd97009e049
LielaisG <fridenbergs.gatis@gmail.com>
2023-07-20 12:49:37 +0300

    Timer driver template

---
+++ custom_board_drivers/Inc/timer.h
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * @file    timer.h
+ * @author  Gatis Fridenbergs
+ * @brief   Header file for timer driver module
+ ******************************************************************************
+ * @attention
+ * Copyright (c) 2023 LielaisG.
+ * https://github.com/LielaisG
+ * All rights reserved.
+ *****************************************************************************/
+
+#ifndef CUSTOM_BOARD_DRIVERS_TIMER_H_
+#define CUSTOM_BOARD_DRIVERS_TIMER_H_
+
+#include "app.h"
+
+/**
+ * @defgroup configuration parameters
+ * @{
+*/
+
+/**
+ * @}
+*/
+
+/**
+ * @defgroup Function declarations
+ * @{
+*/
+
+/**
+ * @}
+*/
+
+#endif /* CUSTOM_BOARD_DRIVERS_TIMER_H_ */
---
+++ custom_board_drivers/Src/timer.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * @file    timer.c
+ * @author  Gatis Fridenbergs
+ * @brief   Timer driver module
+ ******************************************************************************
+ * @attention
+ * Copyright (c) 2023 LielaisG.
+ * https://github.com/LielaisG
+ * All rights reserved.
+ *****************************************************************************/
+
+#include "timer.h"
+
+/**
+ * @brief   
+ * @retval  
+ * @note    
+ * 
+ * @param[in] 
+ * @param[in] 
+*/
