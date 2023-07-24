909d4a51638812a72b17fc49efdb336837d475b0
LielaisG <fridenbergs.gatis@gmail.com>
2023-07-24 08:55:39 +0300

    ADC driver

---
+++ custom_board_drivers/Inc/adc.h
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * @file    adc.h
+ * @author  Gatis Fridenbergs
+ * @brief   Header file for ADC driver module
+ ******************************************************************************
+ * @attention
+ * Copyright (c) 2023 LielaisG.
+ * https://github.com/LielaisG
+ * All rights reserved.
+ *****************************************************************************/
+
+#ifndef CUSTOM_BOARD_DRIVERS_ADC_H_
+#define CUSTOM_BOARD_DRIVERS_ADC_H_
+
+#include "app.h"
+
+/**
+ * @defgroup    Defines for ADC configuration
+ * @{
+*/
+#define CLK_SRC_ADC_FREQ            20000000                    // CLK_SRC_ADC
+#define CLK_ADC_FREQ                10000000                    // CLK_ADC - 10MHz max in normal mode
+/*ADC Input 0*/
+#define IADC_INPUT_0_PORT_PIN       iadcPosInputPortAPin7       //PA07 - OA_IN_P
+#define IADC_INPUT_0_BUS            ABUSALLOC                   //A Bus allocation 
+#define IADC_INPUT_0_BUSALLOC       GPIO_ABUSALLOC_AEVEN0_ADC0  //Mode ADC0 for GPIO_ABUSALLOC
+/*ADC Input 1*/
+#define IADC_INPUT_1_PORT_PIN       iadcNegInputPortAPin8       //PA08 - OA_IN_N
+#define IADC_INPUT_1_BUS            ABUSALLOC                   //A Bus allocation 
+#define IADC_INPUT_1_BUSALLOC       GPIO_ABUSALLOC_AODD0_ADC0   //Shifted mode ADC0 for GPIO_ABUSALLOC
+/**
+ * @}
+*/
+
+/**
+ * @defgroup Global variables
+ * @{
+*/
+static volatile int32_t     sample;
+static volatile double      singleResult; // Volts
+/**
+ * @}
+*/
+
+/**
+ * @defgroup Function declarations
+ * @{
+*/
+void iadc_init(void);
+/**
+ * @}
+*/
+
+#endif /* CUSTOM_BOARD_DRIVERS_ADC_H_ */
---
+++ custom_board_drivers/Src/adc.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * @file    adc.c
+ * @author  Gatis Fridenbergs
+ * @brief   ADC driver module
+ ******************************************************************************
+ * @attention
+ * Copyright (c) 2023 LielaisG.
+ * https://github.com/LielaisG
+ * All rights reserved.
+ *****************************************************************************/
+
+#include "adc.h"
+
+/**
+ * @brief   Function to initialize IADC
+ * @retval  None
+ * @note    
+ * 
+ * @param[in] 
+ * @param[in] 
+*/
+void iadc_init(void)
+{
+    
+}
